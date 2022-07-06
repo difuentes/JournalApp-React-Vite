5d7310459f1d2c0eff4a9d89ea3c858cd80b9a9c
Difuentes <fuentesdiego054@gmail.com>
2022-07-06 13:51:08 -0400

    layout auth

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,22 +1,11 @@
-import {Grid, TextField, Typography} from "@mui/material";
+import {Link as RouterDom } from 'react-router-dom';
+import {Grid, TextField, Typography,Button,Link} from "@mui/material";
+import { Google } from "@mui/icons-material";
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
     return ( 
-       <Grid 
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{minHeight:'100vh',backgroundColor:'primary.main',padding:4}}
-       >
-        
-        <Grid item
-            className='box-shadow'
-            xs={3} 
-            sx={{backgroundColor:'white',padding:3,borderRadius:2}}
-        >
-            <Typography variant="h5" sx={{mb:1,textAlign:'center'}}>Login</Typography>
+       <AuthLayout title="login">
             <form>
                 <Grid container>
                     <Grid item xs={12} sx={{mt:2}}>
@@ -34,13 +23,37 @@ export const LoginPage = () => {
                             label="Contraseña" 
                             type="password" 
                             fullWidth
-                           
                         />
                     </Grid>
+            
+                    <Grid container spacing={2} sx={{mb:2,mt:1}}>
+                        <Grid item xs={12} sm={6}>
+                            <Button sx={{color:'white'}} variant='contained' fullWidth>
+                                Login
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Button  variant='contained' fullWidth>
+                                <Google sx={{color:'white'}}/>
+                                <Typography sx={{color:'white',ml:1}}>Google</Typography> 
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid 
+                        container 
+                        direction="row" 
+                        justifyContent="end"
+                    >
+                        <Link component={RouterDom} color='inherit' to="/auth/register" >
+                            Crear Cuenta
+                        </Link>
+                        
+                    </Grid>
+
                 </Grid>
             </form>
-        </Grid>
-
-       </Grid>
+       </AuthLayout>
+            
     )
 }
--- src/auth/pages/ResgisterPage.tsx
+++ src/auth/pages/ResgisterPage.tsx
@@ -1,8 +1,58 @@
-
+import { AuthLayout } from "../layout/AuthLayout"
+import { Grid, TextField, Button, Typography ,Link} from '@mui/material';
+import {Link as RouterDom } from 'react-router-dom';
 export const ResgisterPage = () => {
     return (
-        <div>
-            ResgisterPage
-        </div>
+       <AuthLayout title="Crear Cuenta">
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{mt:2}}>
+                        <TextField 
+                            label="Nombre completo" 
+                            type="text" 
+                            placeholder="Nombre"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item  xs={12} sx={{mt:2}}>
+                        <TextField 
+                            label="Contraseña" 
+                            type="password" 
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item  xs={12} sx={{mt:2}}>
+                        <TextField 
+                            label="Contraseña" 
+                            type="password" 
+                            fullWidth
+                           
+                        />
+                    </Grid>
+
+                    <Grid container spacing={2} sx={{mb:2,mt:1}}>
+                        <Grid item xs={12} >
+                            <Button sx={{color:'white'}} variant='contained' fullWidth>
+                                Crear Cuenta
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid 
+                        container 
+                        direction="row" 
+                        justifyContent="end"
+                    >
+                        <Typography sx={{mr:1,color:'primary.main',fontWeight:'bold'}}>¿Ya tienes cuenta?</Typography>
+                        <Link component={RouterDom} color='inherit' to="/auth/login" >
+                            Ingresar
+                        </Link>
+                        
+                    </Grid>
+
+                </Grid>
+            </form>
+       </AuthLayout>
     )
 }
